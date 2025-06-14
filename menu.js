1fb4035daa88d16a016178e9c8d315a4d07c342e
JoyBoywz <marcellosoaresbraz@gmail.com>
2025-06-14 12:25:12 -0300

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,6 @@
+const togglemenu = document.querySelu-ector('mentoggle');
+const menutopo = document.querySelector('menutopo');
+
+menutoggle.addEventListener('click', () => {
+  menutopo.classList.toggle('ativo');
+});
